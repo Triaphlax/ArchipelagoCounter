2f237d792858b5accdbc777bf35315c61fae2bd4
Triaphlax <rolf.plagmeijer@gmail.com>
2026-09-19 00:16:01 +0200

    Pending list highlights current game items blue.

--- LayoutTool/PendingItems/LayoutTool_PendingItems.gd
+++ LayoutTool/PendingItems/LayoutTool_PendingItems.gd
@@ -1,14 +1,12 @@
 class_name LayoutTool_PendingItems
 extends PanelContainer
 
+@export var panel_style_selected_even: StyleBox
+@export var panel_style_selected_odd: StyleBox
 @export var panel_style_even: StyleBox
 @export var panel_style_odd: StyleBox
 
-var scroll1: ScrollContainer
-var scroll2: ScrollContainer
-var scroll3: ScrollContainer
-var scroll4: ScrollContainer
-var scrolls: Array
+var scroll: ScrollContainer
 var rows_that_fit: int
 
 # relates slots to a dictionary relating logical advancement items to their counts, for items that have not yet been used
@@ -19,20 +17,14 @@ func _ready() -> void:
 	Counter.update.connect(generate_grid)
 	
 	var hbox = get_node("HBoxContainer")
-	scroll1 = hbox.get_node("ScrollContainer")
-	scroll2 = hbox.get_node("ScrollContainer2")
-	scroll3 = hbox.get_node("ScrollContainer3")
-	scroll4 = hbox.get_node("ScrollContainer4")
-	scrolls = [scroll1, scroll2, scroll3, scroll4]
+	scroll = hbox.get_node("ScrollContainer")
 	
 	await Counter.loaded()
-	
-	rows_that_fit = floor(scroll1.size.y / 30)
 	
 	for log_entry in Counter.save.log:
 		update_pending_items_list(log_entry)
 	
-	generate_grid(rows_that_fit, scrolls)
+	generate_grid()
 
 
 func log_received(log_message: LogMessage):
@@ -70,37 +62,41 @@ func update_pending_items_list(log_message: LogMessage):
 				pending_items.erase(player_name)
 
 
-func generate_grid(itemsPerScroll: int, scrolls: Array):
+func generate_grid():
+	# check is connected here
+	var grid = scroll.get_node("GridContainer")
 	var count := 0
-	var grid = scrolls[0].get_node("GridContainer")
+	var panel_style
 	for child in grid.get_children():
 		child.queue_free()
+		
+	for playerId in Counter.active_players:
+		var player = Counter.get_slot_from_id(playerId)
+		for item in pending_items[player].keys():
+			panel_style = panel_style_selected_even if count % 2 == 0 else panel_style_selected_odd
+			add_to_grid(player, item, grid, panel_style)
+			count += 1
 	
 	for player in pending_items.keys():
+		if Counter.get_slot_id_from_name(player) in Counter.active_players:
+			continue
 		for item in pending_items[player].keys():
-			var player_entry := LayoutTool_PendingItems_Entry.instantiate(player, Counter.get_color_for_slot(player), true)
-			var item_entry := LayoutTool_PendingItems_Entry.instantiate(item, Counter.settings.log_item_color, true, HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT)
-			var count_text := "x" + str(pending_items[player][item]) if pending_items[player][item] > 1 else ""
-			var count_entry := LayoutTool_PendingItems_Entry.instantiate(count_text, Color.WHITE, false)
-			
-			var panel_style := panel_style_even if count % 2 == 0 else panel_style_odd
-			item_entry.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-			player_entry.add_theme_stylebox_override(&"panel", panel_style)
-			item_entry.add_theme_stylebox_override(&"panel", panel_style)
-			count_entry.add_theme_stylebox_override(&"panel", panel_style)
-			
-			grid.add_child(player_entry)
-			grid.add_child(item_entry)
-			grid.add_child(count_entry)
-			
+			panel_style = panel_style_even if count % 2 == 0 else panel_style_odd
+			add_to_grid(player, item, grid, panel_style)
 			count += 1
-			
-	#var allGridItems = grid.get_children()
-	#var maxGridItems = len(allGridItems)
-	#var scrollNo = 0
-	#for scroll in scrolls.slice(1):
-		#scrollNo += 1
-		#var extraGrid = scroll.get_node("GridContainer")
-		#for i in range(scrollNo * itemsPerScroll, min((scrollNo+1) * itemsPerScroll, maxGridItems)):
-			#allGridItems[i].reparent(extraGrid, false)
-		
+
+
+func add_to_grid(player, item, grid, panel_style):
+	var player_entry := LayoutTool_PendingItems_Entry.instantiate(player, Counter.get_color_for_slot(player), true)
+	var item_entry := LayoutTool_PendingItems_Entry.instantiate(item, Counter.settings.log_item_color, true, HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT)
+	var count_text := "x" + str(pending_items[player][item]) if pending_items[player][item] > 1 else ""
+	var count_entry := LayoutTool_PendingItems_Entry.instantiate(count_text, Color.WHITE, false)
+	
+	item_entry.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	player_entry.add_theme_stylebox_override(&"panel", panel_style)
+	item_entry.add_theme_stylebox_override(&"panel", panel_style)
+	count_entry.add_theme_stylebox_override(&"panel", panel_style)
+	
+	grid.add_child(player_entry)
+	grid.add_child(item_entry)
+	grid.add_child(count_entry)
