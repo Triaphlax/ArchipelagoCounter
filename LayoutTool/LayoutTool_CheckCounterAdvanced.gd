de3be903519014ab5d878b2d532788f9ee5580f6
Triaphlax <rolf.plagmeijer@gmail.com>
2026-09-19 22:17:01 +0200

    Fix timers

--- LayoutTool/LayoutTool_CheckCounterAdvanced.gd
+++ LayoutTool/LayoutTool_CheckCounterAdvanced.gd
@@ -17,9 +17,9 @@ func update():
 		modulate.a = 0.0
 	else:
 		modulate.a = 1.0
-		update_text(Counter.game_checks[game], Counter.total_game_checks[game])
+		update_text(game, Counter.game_checks[game], Counter.total_game_checks[game])
 
-func update_text(checks: int, total_checks: int):
+func update_text(game: String, checks: int, total_checks: int):
 	var total_checks_digits := len(str(total_checks))
 	var checks_str := str(checks).pad_zeros(total_checks_digits)
 	var percent := str((float(checks) / float(total_checks)) * 100)
@@ -28,6 +28,8 @@ func update_text(checks: int, total_checks: int):
 	
 	#var cpm := str(60.0 * (float(checks) / Counter.save.timer))
 	#cpm = cpm.pad_decimals(4)
-	
-	text = text_format.format([checks, total_checks, percent, Utils.seconds_to_hms(Counter.save.timer)])
+	var game_time := 0
+	if game in Counter.save.game_timer.keys():
+		game_time = Counter.save.game_timer[game]
+	text = text_format.format([checks, total_checks, percent, Utils.seconds_to_hms(game_time)])
 		
